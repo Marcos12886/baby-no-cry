d8f7168d0edb77e155338f138534130e182e2741
Marcos12886 <marcos.cenzual8@gmail.com>
2024-12-01 20:46:25 +0100

    Modificar comentarios...

--- app.py
+++ app.py
@@ -6,34 +6,34 @@ from model import predict_params, AudioDataset
 load_dotenv()
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model_class, id2label_class = predict_params(
-    model_path="distilhubert-finetuned-mixed-data", # Ruta al modelo para la predicción de clases de llanto
+    model_path="distilhubert-finetuned-mixed-data", # Ruta al modelo para la predicción de clases
     dataset_path="data/mixed_data", # Ruta al dataset de audio mixto
     undersample_normal=True # Activa el submuestreo para equilibrar clases
     )
 model_detec, _ = predict_params(
-    model_path="distilhubert-finetuned-cry-detector", # Ruta al modelo detector de llanto
-    dataset_path="data/baby_cry_detection", # Ruta al dataset de detección de llanto
+    model_path="distilhubert-finetuned-cry-detector", # Ruta al modelo detector
+    dataset_path="data/baby_cry_detection", # Ruta al dataset de detección
     undersample_normal=False # No submuestrear datos
     )
 
 def call(audiopath, model, dataset_path, undersample_normal=False):
     model.to(device) # Envía el modelo a la GPU (o CPU si no hay GPU disponible)
     model.eval() # Pone el modelo en modo de evaluación (desactiva dropout, batchnorm)
-    audio_dataset = AudioDataset(dataset_path, {}, undersample_normal) # Carga el dataset de audio con parámetros específicos
-    processed_audio = audio_dataset.preprocess_audio(audiopath) # Preprocesa el audio según la configuración del dataset
+    audio_dataset = AudioDataset(dataset_path, {}, undersample_normal) # Carga dataset de audio
+    processed_audio = audio_dataset.preprocess_audio(audiopath) # Preprocesa el audio
     inputs = {"input_values": processed_audio.to(device).unsqueeze(0)}
-    with torch.no_grad(): # Desactiva el cálculo del gradiente para ahorrar memoria
-        outputs = model(**inputs) # Realiza la inferencia con el modelo
-        logits = outputs.logits # Obtiene las predicciones del modelo
-    return logits # Retorna los logits (valores sin procesar)
+    with torch.no_grad(): # Desactivar cálculo del gradiente para ahorrar memoria
+        outputs = model(**inputs) # Inferir
+        logits = outputs.logits # Obtener las predicciones
+    return logits # Logits (valores sin procesar)
 
 def predict(audio_path_pred):
     with torch.no_grad():
         logits = call(audio_path_pred, model=model_class, dataset_path="data/mixed_data", undersample_normal=False)
         predicted_class_ids_class = torch.argmax(logits, dim=-1).item() # Obtiene la clase predicha a partir de los logits
         label_class = id2label_class[predicted_class_ids_class] # Convierte el ID de clase en una etiqueta de texto
-        label_mapping = {0: 'Cansancio/Incomodidad', 1: 'Dolor', 2: 'Hambre', 3: 'Problemas para respirar'} # Mapea las etiquetas
-        label_class = label_mapping.get(predicted_class_ids_class, label_class) # Si hay una etiqueta personalizada, la usa
+        label_mapping = {0: 'Cansancio/Incomodidad', 1: 'Dolor', 2: 'Hambre', 3: 'Problemas para respirar'} # Mapear etiquetas
+        label_class = label_mapping.get(predicted_class_ids_class, label_class) # Aplicar las etiquetas distintas
     return f"""
         <div style='text-align: center; font-size: 1.5em'>
             <span style='display: inline-block; min-width: 300px;'>{label_class}</span>
@@ -41,22 +41,22 @@ def predict(audio_path_pred):
     """
 
 def predict_stream(audio_path_stream):
-    with torch.no_grad(): # Desactiva gradientes durante la inferencia
+    with torch.no_grad(): # Desactivar gradientes durante inferencia
         logits = call(audio_path_stream, model=model_detec, dataset_path="data/baby_cry_detection", undersample_normal=False)
-        probabilities = torch.nn.functional.softmax(logits, dim=-1) # Aplica softmax para convertir los logits en probabilidades
-        crying_probabilities = probabilities[:, 1] # Obtiene las probabilidades asociadas al llanto
-        avg_crying_probability = crying_probabilities.mean()*100 # Calcula la probabilidad promedio de llanto
-        if avg_crying_probability < 15: # Si la probabilidad de llanto es menor a un 15%, se predice la razón
-            label_class = predict(audio_path_stream) # Llama a la predicción para determinar la razón del llanto
-            return f"Está llorando por: {label_class}" # Retorna el resultado indicando por qué llora
+        probabilities = torch.nn.functional.softmax(logits, dim=-1) # Softmax para convertir logits en probabilidades
+        crying_probabilities = probabilities[:, 1] # Obtener probabilidades
+        avg_crying_probability = crying_probabilities.mean()*100 # Probabilidad media de llanto
+        if avg_crying_probability < 15: # Si probabilidad de predicción > 15 dar la razón
+            label_class = predict(audio_path_stream) # Predicción para determinar la razón
+            return f"Está llorando por: {label_class}" # Razon de lloro
         else:
             return "No está llorando"
 
 def chatbot_config(history, type='messages'):
-    system_message = "You are a Chatbot specialized in baby health and care." # Mensaje inicial del chatbot
-    max_tokens = 512 # Máximo de tokens para la respuesta
-    messages = [system_message] # Configura el mensaje del sistema para el chatbot
-    for val in history: # Añade el historial de la conversación al mensaje
+    system_message = "You are a Chatbot specialized in baby health and care."
+    max_tokens = 512 # Máximo de tokens por respuesta
+    messages = [system_message] # Mensaje del sistema del chatbot
+    for val in history: # Añade el historial de la conversación
         if val[0]:
             messages.append(val[0]) # Añade los mensajes del usuario
         if val[1]:
@@ -75,18 +75,18 @@ def chatbot_config(history, type='messages'):
     return response
 
 def cambiar_pestaña():
-    return gr.update(visible=False), gr.update(visible=True) # Esta función cambia la visibilidad de las pestañas en la interfaz
+    return gr.update(visible=False), gr.update(visible=True) # Cambiar que pagina mostrar
 
 my_theme = gr.themes.Soft(
-    primary_hue="lime",  # Light purple for a calming effect
+    primary_hue="lime",
     neutral_hue="slate",
     text_size="sm",
     spacing_size="sm",
     font=[gr.themes.GoogleFont('Bubblegum Sans'), 'ui-sans-serif', 'system-ui', 'sans-serif'],
     font_mono=[gr.themes.GoogleFont('Bubblegum Sans'), 'ui-monospace', 'Consolas', 'monospace'],
 ).set(
-    body_background_fill='*neutral_100',  # Lighter background
-    body_text_color='*neutral_700',  # Slightly darker text for better readability
+    body_background_fill='*neutral_100',
+    body_text_color='*neutral_700',
     body_text_size='*text_sm',
     embed_radius='*radius_md',
     shadow_drop='*shadow_spread',
@@ -94,16 +94,15 @@ my_theme = gr.themes.Soft(
 )
 
 with gr.Blocks(theme=my_theme, fill_height=True, fill_width=True) as demo:
-    with gr.Column(visible=True) as chatbot: # Columna para la pestaña del chatbot
-        gr.Markdown("<h2>Asistente</h2>") # Título de la pestaña del chatbot
+    with gr.Column(visible=True) as chatbot:
+        gr.Markdown("<h2>Asistente</h2>") # Título chatbot
         gr.Markdown(
             "<h4 style='text-align: center;"
             "font-size: 1.5em'>"
             "Pregunta a nuestro asistente cualquier duda que tengas sobre el cuidado de tu bebé</h4>"
         )
         gr.ChatInterface(
-            chatbot_config, # Función de configuración del chatbot
-            theme=my_theme, # Tema personalizado para la interfaz
+            fn=chatbot_config, # Función de configuración del chatbot
             submit_btn="Enviar", # Botón de enviar mensaje
             autofocus=True, # Enfocar automáticamente el campo de entrada de texto
             fill_height=True, # Rellenar el espacio verticalmente
@@ -129,11 +128,11 @@ with gr.Blocks(theme=my_theme, fill_height=True, fill_width=True) as demo:
             type="filepath", # Tipo de entrada de audio (archivo)
         )
         gr.Button("¿Por qué llora?").click(
-            predict, # Función de predicción para el botón
+            fn=predict, # Función de predicción para el botón
             inputs=audio_input, # Entrada de audio para la función de predicción
             outputs=gr.Markdown() # Salida para mostrar la predicción
         )
-        gr.Button("Volver").click(cambiar_pestaña, outputs=[pag_predictor, chatbot]) # Botón para volver a la pestaña del chatbot
+        gr.Button("Volver").click(cambiar_pestaña, outputs=[pag_predictor, chatbot]) # Mostrar chatbot
     with gr.Column(visible=False) as pag_monitor: # Columna para la pestaña del monitor
         gr.Markdown("<h2>Monitor</h2>") # Título de la pestaña del monitor
         gr.Markdown(
@@ -148,11 +147,11 @@ with gr.Blocks(theme=my_theme, fill_height=True, fill_width=True) as demo:
             type="filepath", # Tipo de entrada de audio (archivo)
         )
         audio_stream.stream(
-            predict_stream, # Función para realizar la predicción en tiempo real
+            fn=predict_stream, # Función para realizar la predicción en tiempo real
             inputs=audio_stream, # Entradas para la función de predicción en tiempo real
             outputs=gr.Markdown() # Salida para mostrar la predicción en tiempo real
         )
-        gr.Button("Volver").click(cambiar_pestaña, outputs=[pag_monitor, chatbot]) # Botón para volver a la pestaña del chatbot
-    boton_predictor.click(cambiar_pestaña, outputs=[chatbot, pag_predictor]) # Botón para cambiar a la pestaña del predictor
-    boton_monitor.click(cambiar_pestaña, outputs=[chatbot, pag_monitor]) # Botón para cambiar a la pestaña del monitor
+        gr.Button("Volver").click(cambiar_pestaña, outputs=[pag_monitor, chatbot]) # Mostrar chatbot
+    boton_predictor.click(cambiar_pestaña, outputs=[chatbot, pag_predictor]) # Mostrar predictor
+    boton_monitor.click(cambiar_pestaña, outputs=[chatbot, pag_monitor]) # Mostrar monitor
 demo.launch() # Lanzar la interfaz gráfica
